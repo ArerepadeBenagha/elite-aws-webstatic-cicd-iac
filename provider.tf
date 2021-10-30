22b09343560b0fe08abb06f9e5af5b5703014f26
ArerepadeB <aredelulu@gmail.com>
2021-10-30 01:24:44 -0500

    testing tfc

--- provider.tf
+++ provider.tf
@@ -7,18 +7,18 @@ terraform {
     }
   }
 }
-provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region     = var.aws_region
-}
+# provider "aws" {
+#   access_key = var.AWS_ACCESS_KEY_ID
+#   secret_key = var.AWS_SECRET_ACCESS_KEY
+#   region     = var.aws_region
+# }
 
-provider "aws" {
-  alias      = "acm_provider"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region     = var.aws_region
-}
+# provider "aws" {
+#   alias      = "acm_provider"
+#   access_key = var.AWS_ACCESS_KEY_ID
+#   secret_key = var.AWS_SECRET_ACCESS_KEY
+#   region     = var.aws_region
+# }
 terraform {
   backend "remote" {
 
