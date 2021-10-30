b5fa4f262650e462eed9a8a935398f3a74ef94cb
ArerepadeB <aredelulu@gmail.com>
2021-10-30 01:15:30 -0500

    testing tfc

--- variables.tf
+++ variables.tf
@@ -8,8 +8,8 @@ variable "bucket_name" {
   description = "The name of the bucket without the www. prefix. Normally domain_name."
   default     = "elietesolutionsit.de"
 }
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+# variable "AWS_ACCESS_KEY_ID" {}
+# variable "AWS_SECRET_ACCESS_KEY" {}
 variable "aws_region" {
   default = "us-east-1"
 }
