60488d3ed6ff80ddaed91a721ab2ed269123bc39
ArerepadeB <aredelulu@gmail.com>
2021-10-30 01:14:17 -0500

    testing tfc

--- provider.tf
+++ provider.tf
@@ -8,14 +8,24 @@ terraform {
   }
 }
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = var.aws_region
 }
 
 provider "aws" {
   alias      = "acm_provider"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = var.aws_region
+}
+terraform {
+  backend "remote" {
+
+   organization = "EliteSolutionsIT"
+
+    workspaces {
+      name = "githubactions-aws-static-s3"
+    }
+  }
 }
